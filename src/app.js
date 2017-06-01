8604a643fecb594e3a9c5e64ed006f9b567d4e84
sroyz <sroyz@cisco.com>
2017-06-01 18:28:21 +0200

    introducing react

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Component = () => (
+	<h1>Hello world</h1>
+	);
+
+ReactDOM.render(
+  <Component />,
+  document.getElementById('root')
+);
