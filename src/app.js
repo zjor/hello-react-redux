5c668d1668794b54f8898461cb66e7793c65774c
sroyz <sroyz@cisco.com>
2017-06-02 14:21:30 +0200

    implementing counter using native state

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Component = () => (
-	<h1>Hello world</h1>
-	);
+const Button = (props) => {
+	return (
+		<button onClick={props.onClick}>{props.caption}</button>
+		);
+};
+
+const Label = (props) => (<span>{props.counter}</span>)
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {counter: 0};	
+	}
+
+	render() {
+
+		const increment = () => {
+			this.setState({counter: this.state.counter + 1});
+		};
+
+		const decrement = () => {
+			this.setState({counter: this.state.counter - 1});
+		};
+
+		return (
+			<div>
+				<h1>Hello world</h1>
+				<Button caption="+" onClick={increment} />
+				<Label counter={this.state.counter} />
+				<Button caption="-" onClick={decrement} />
+			</div>
+			);
+	}
+};
 
 ReactDOM.render(
-  <Component />,
+  <App />,
   document.getElementById('root')
 );
