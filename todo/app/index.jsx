8e8777e3296bf902b0f24af2b992f04e3f2f5e09
zjor <zjor.se@gmail.com>
2017-06-15 01:01:19 +0200

    add task component created

--- todo/app/index.jsx
+++ todo/app/index.jsx
@@ -20,18 +20,38 @@ const TaskList = ({tasks}) => {
 class AddTask extends React.Component {
 
     constructor(props) {
-        super(props);
+        super(props);        
+    }
+
+    componentWillMount() {
         this.setState({typed: ''});
     }
 
+    keyPressed(event) {
+        this.setState({
+            typed: event.target.value
+        });
+    }
+
+    keyDown(event) {
+        if (event.key == 'Enter') {
+            this.submitValue();
+        }
+    }
+
+    submitValue() {
+        this.props.onValue(this.state.typed);
+        this.textInput.value = '';
+    }
+
     render() {
-
-        const handler = (event) => this.setState({typed: event.target.value});
-
         return (
             <div>
-                <input type="text" placeholder="Enter task here" onChange={handler.bind(this)}/>
-                <button>Add</button>
+                <input type="text" placeholder="Enter task here" 
+                    onChange={this.keyPressed.bind(this)} 
+                    onKeyDown={this.keyDown.bind(this)}
+                    ref={(input => { this.textInput = input; })}/>
+                <button onClick={this.submitValue.bind(this)}>Add</button>
             </div>
         );
     }
@@ -39,10 +59,11 @@ class AddTask extends React.Component {
 }
 
 const tasks = ["One", "Two", "Three"];
+const onValue = (value) => console.log(value);
 
 render(
     <App>
-        <AddTask/>
+        <AddTask onValue={onValue}/>
         <TaskList tasks={tasks}/>
     </App>,
     document.getElementById('root'));
