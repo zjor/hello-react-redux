1c7bf8a465cb551b3437efbfa507c1f84b6aa767
sroyz <sroyz@cisco.com>
2017-06-16 17:58:44 +0200

    filtering was added

--- todo/app/components/App.jsx
+++ todo/app/components/App.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { AddTask } from 'components/AddTask';
-import { TaskList } from 'components/TaskList';
+import {AddTask} from 'components/AddTask';
+import {Filter} from 'components/Filter';
+import {TaskList} from 'components/TaskList';
 
 class App extends Component {
 
@@ -18,7 +19,11 @@ class App extends Component {
                 done: false
             });
         });
-        this.setState({tasks: tasks});
+        this.setState({
+            tasks: tasks,
+            visibleTasks: tasks,
+            filter: this.props.filter
+        });
     }
 
     getNextId() {
@@ -27,33 +32,63 @@ class App extends Component {
 
     render() {
 
+        const filterTasks = (tasks, filter) => {
+            if (filter === 'All') {
+                return tasks;
+            } else if (filter === 'Done') {
+                return tasks.filter(task => task.done);
+            } else if (filter === 'Active') {
+                return tasks.filter(task => !task.done);
+            }
+        };
+
         const onValue = (value) => {
-            this.setState({tasks: this.state.tasks.concat({
+            const tasks = this.state.tasks.concat({
                 id: this.getNextId(),
                 name: value,
                 done: false
-            })});
+            });
+
+            this.setState({
+                tasks: tasks,
+                visibleTasks: filterTasks(tasks, this.state.filter),
+                filter: this.state.filter
+            });
         };
 
         const onToggleDone = (task) => {
             let tasks = [];
             this.state.tasks.forEach(t => {
-                if (t.id == task.id) {
-                    t.done = !t.done;
-                }
-                tasks.push(t);
+                tasks.push({
+                    id: t.id,
+                    name: t.name,
+                    done: t.id === task.id ? !t.done : t.done
+                });
             });
-            this.setState({tasks: tasks});
+            this.setState({
+                tasks: tasks,
+                visibleTasks: filterTasks(tasks, this.state.filter),
+                filter: this.state.filter
+            });
+        };
+
+        const onFilter = (name) => {
+            this.setState({
+                tasks: this.state.tasks,
+                visibleTasks: filterTasks(this.state.tasks, name),
+                filter: name
+            });
         };
 
         return (
             <div>
                 <AddTask onValue={onValue}/>
-                <TaskList tasks={this.state.tasks} onToggleDone={onToggleDone}/>
+                <Filter filterName={this.state.filter} onChange={onFilter}/>
+                <TaskList tasks={this.state.visibleTasks} onToggleDone={onToggleDone}/>
             </div>
         );
     }
 
 }
 
-export { App };
+export {App};
--- todo/app/index.jsx
+++ todo/app/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import { App } from 'components/App';
+import {App} from 'components/App';
 
-render(<App tasks={["Go to work", "Buy milk", "Jogging", "Go to bed"]}/>, document.getElementById('root'));
+render(
+    <App
+        tasks={["Go to work", "Buy milk", "Jogging", "Go to bed"]}
+        filter="All"
+    />, document.getElementById('root'));
